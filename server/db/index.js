208632016efa4cf694982cc23f18b96ebd57e33b
GMV44 <root@LAPTOP-B6VQPMKU.localdomain>
2023-01-10 16:29:04 -0600

    added some tables

--- server/db/index.js
+++ server/db/index.js
@@ -3,12 +3,21 @@
 const db = require('./db')
 
 const User = require('./models/User')
+const Products = require('./models/Products')
+const Cart = require('./models/Cart')
 
 //associations could go here!
+User.hasOne(Cart);
+Cart.hasOne(User);
+Cart.hasMany(Products);
+Products.belongsTo(Cart);
+
 
 module.exports = {
   db,
   models: {
     User,
+    Products,
+    Cart
   },
 }
