ead9544490fa930133567ca523e37398a6415c1b
Kyle Colangelo <kyle123c@hotmail.com>
2023-01-11 11:58:02 -0500

    api route changes
    Co-authored-by: Mstone95 <Mstone95@users.noreply.github.com>
    Co-authored-by: keenan-urdiales <keenan-urdiales@users.noreply.github.com>

--- server/api/users.js
+++ server/api/users.js
@@ -2,15 +2,60 @@ const router = require('express').Router()
 const { models: { User }} = require('../db')
 module.exports = router
 
+// GET /api/users
 router.get('/', async (req, res, next) => {
   try {
     const users = await User.findAll({
       // explicitly select only the id and username fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
-      attributes: ['id', 'username']
+      attributes: ['id', 'username', 'firstName', 'lastName', 'email', 'phoneNumber', 'address']
     })
     res.json(users)
+  } catch (err) {
+    next(err)
+  }
+})
+
+// GET /api/users/:userId
+router.get('/:userId', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.userId, {
+      attributes: ['id', 'username', 'firstName', 'lastName', 'email', 'phoneNumber', 'address']
+    })
+    res.json(user)
+  } catch (err) {
+    next(err)
+  }
+})
+
+// POST /api/users
+router.post('/', async (req, res, next) => {
+  try {
+    const user = await User.create(req.body)
+    res.status(201).json(user)
+  } catch (err) {
+    next(err)
+  }
+})
+
+// PUT /api/users/:userId
+router.put('/:userId', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.userId)
+    await user.update(req.body)
+    res.json(user)
+  } catch (err) {
+    next(err)
+  }
+})
+
+// DELETE /api/users/:userId
+router.delete('/:userId', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.userId)
+    await user.destroy()
+    res.json(user)
   } catch (err) {
     next(err)
   }
