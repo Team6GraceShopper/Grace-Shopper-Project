746b35caa2032bab0a8ecf4788f586c3f3e5b0ea
keenan-urdiales <keenanurdiales@icloud.com>
2023-01-18 08:20:24 -0700

    cart slices

--- client/app/store.js
+++ client/app/store.js
@@ -2,9 +2,10 @@ import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import authReducer from '../features/account/authSlice';
 import { signupSlice } from '../features/signup/signupSlice';
+import {cartReducer} from '../features/shop/cartSlice'
 
 const store = configureStore({
-  reducer: { auth: authReducer, theSignup: signupSlice },
+  reducer: { auth: authReducer, theSignup: signupSlice, reducer: cartReducer },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
